5ae697b20895e8aac44718254e2a6ff205bfaaee
Ademir Júnior <adejr0220@gmail.com>
2024-12-28 10:08:37 -0300

    :sparkles: feat: edit tasks with JavaScript

--- script.js
+++ script.js
@@ -3,26 +3,31 @@ document.addEventListener("DOMContentLoaded", function () {
   if (!localStorage.getItem("tasks")) {
     const preTasks = [
       {
+        id: 1,
         name: "Revisar proposta do cliente",
         description: "Analisar a proposta enviada pelo cliente para garantir que todos os requisitos foram cobertos.",
         conclusionDate: "2024-12-30",
       },
       {
+        id: 2,
         name: "Preparar relatório mensal",
         description: "Criar um relatório com os principais indicadores de desempenho do mês.",
         conclusionDate: "2025-01-03",
       },
       {
+        id: 3,
         name: "Atualizar documentação do projeto",
         description: "Revisar e atualizar a documentação técnica com as mudanças feitas na última sprint.",
         conclusionDate: "2025-01-05",
       },
       {
+        id: 4,
         name: "Planejar reunião de equipe",
         description: "Organizar a pauta e os materiais necessários para a reunião de planejamento trimestral.",
         conclusionDate: "2025-01-02",
       },
       {
+        id: 5,
         name: "Enviar orçamento para novo cliente",
         description: "Preparar e enviar o orçamento solicitado pelo cliente potencial.",
         conclusionDate: "2024-12-31",
@@ -33,58 +38,59 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-
 // Create new task
-const openModal = document.querySelector("#add-task");
-const closeModal = document.querySelector("#close-modal");
+const openCreateModal = document.querySelector("#add-task-button");
+const closeCreateModal = document.querySelector("#close-create-modal");
 const createTaskModal = document.querySelector("#create-task-modal");
 const createTaskForm = document.querySelector("#create-task-form");
-const taskTitleInput = document.querySelector("#task-title-input");
-const tasktTitleError = document.querySelector("#title-error");
-const taskDescriptionInput = document.querySelector("#task-description-input");
-const taskDescriptionError = document.querySelector("#description-error");
-const taskConclusionDateInput = document.querySelector("#task-conclusion-date-input");
-const taskConclusionDateError = document.querySelector("#conclusion-date-error");
-const createTaskButton = document.querySelector("#create-task-button");
+const createTaskTitleInput = document.querySelector("#create-task-title-input");
+const createTasktTitleError = document.querySelector("#create-title-error");
+const createTaskDescriptionInput = document.querySelector("#create-task-description-input");
+const createTaskDescriptionError = document.querySelector("#create-description-error");
+const createTaskConclusionDateInput = document.querySelector("#create-task-conclusion-date-input");
+const createTaskConclusionDateError = document.querySelector("#create-conclusion-date-error");
 
-taskConclusionDateInput.min = new Date().toISOString().split("T")[0];
+createTaskConclusionDateInput.min = new Date().toISOString().split("T")[0];
 
-openModal.addEventListener("click", toggleTaskModal);
+openCreateModal.addEventListener("click", () => toggleTaskModal("create"));
 
-closeModal.addEventListener("click", toggleTaskModal);
+closeCreateModal.addEventListener("click", () => toggleTaskModal("create"));
 
 createTaskForm.addEventListener("submit", handleCreateNewTask);
 
 function handleCreateNewTask(e) {
   e.preventDefault();
-  const taskTitle = taskTitleInput.value;
-  const taskDescription = taskDescriptionInput.value;
-  const taskConclusionDate = taskConclusionDateInput.value;
+  const taskTitle = createTaskTitleInput.value;
+  const taskDescription = createTaskDescriptionInput.value;
+  const taskConclusionDate = createTaskConclusionDateInput.value;
 
   if (!taskTitle) {
-    tasktTitleError.textContent = "Por favor, digite um título";
+    createTasktTitleError.textContent = "Por favor, digite um título";
   }
 
   if (!taskDescription) {
-    taskDescriptionError.textContent = "Escreva uma breve descrição sobre a tarefa";
+    createTaskDescriptionError.textContent = "Escreva uma breve descrição sobre a tarefa";
   }
 
   if (!taskConclusionDate) {
-    taskConclusionDateError.textContent = "Informe uma data de conclusão";
+    createTaskConclusionDateError.textContent = "Informe uma data de conclusão";
   }
 
   if (!taskTitle || !taskDescription || !taskConclusionDate) {
     return;
   }
 
+  const taskId = JSON.parse(localStorage.getItem("tasks")).length + 1;
+
   const task = {
+    id: taskId,
     name: taskTitle,
     description: taskDescription,
     conclusionDate: taskConclusionDate,
   };
 
   const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  
+
   localStorage.setItem("tasks", JSON.stringify([...tasks, task]));
 
   createTaskForm.reset();
@@ -92,16 +98,104 @@ function handleCreateNewTask(e) {
   window.location.reload();
 }
 
-function toggleTaskModal() {
-  const isOpen = createTaskModal.style.display === "flex";
+// Edit new task
+const closeEditModal = document.querySelector("#close-edit-modal");
+const editTaskModal = document.querySelector("#edit-task-modal");
+const editTaskForm = document.querySelector("#edit-task-form");
+const editTaskTitleInput = document.querySelector("#edit-task-title-input");
+const editTasktTitleError = document.querySelector("#edit-title-error");
+const editTaskDescriptionInput = document.querySelector("#edit-task-description-input");
+const editTaskDescriptionError = document.querySelector("#edit-description-error");
+const editTaskConclusionDateInput = document.querySelector("#edit-task-conclusion-date-input");
+const editTaskConclusionDateError = document.querySelector("#edit-conclusion-date-error");
 
-  tasktTitleError.textContent = "";
-  taskDescriptionError.textContent = "";
-  taskConclusionDateError.textContent = "";
-  
-  createTaskForm.reset();
+editTaskConclusionDateInput.min = new Date().toISOString().split("T")[0];
 
-  createTaskModal.setAttribute("style", isOpen ? "display: none;" : "display: flex;");
+closeEditModal.addEventListener("click", () => toggleTaskModal("edit"));
+
+function handleEditNewTask(editingTask) {
+  const taskTitle = editTaskTitleInput.value;
+  const taskDescription = editTaskDescriptionInput.value;
+  const taskConclusionDate = editTaskConclusionDateInput.value;
+
+  if (!taskTitle) {
+    editTasktTitleError.textContent = "Por favor, digite um título";
+  }
+
+  if (!taskDescription) {
+    editTaskDescriptionError.textContent = "Escreva uma breve descrição sobre a tarefa";
+  }
+
+  if (!taskConclusionDate) {
+    editTaskConclusionDateError.textContent = "Informe uma data de conclusão";
+  }
+
+  if (!taskTitle || !taskDescription || !taskConclusionDate) {
+    return;
+  }
+
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  const updatedTasks = tasks.map((task) => {
+    if (task.id === editingTask.id) {
+      return {
+        ...task,
+        name: taskTitle,
+        description: taskDescription,
+        conclusionDate: taskConclusionDate,
+      };
+    }
+
+    return task;
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+
+  editTaskForm.reset();
+  toggleTaskModal("edit");
+  window.location.reload();
+}
+
+function toggleTaskModal(modalName, task) {
+  if (modalName === "create") {
+    const isCreateModalOpen = createTaskModal.style.display === "flex";
+
+    createTasktTitleError.textContent = "";
+    createTaskDescriptionError.textContent = "";
+    createTaskConclusionDateError.textContent = "";
+
+    createTaskForm.reset();
+
+    createTaskModal.setAttribute("style", isCreateModalOpen ? "display: none;" : "display: flex;");
+
+    return;
+  }
+
+  if (modalName === "edit") {
+    const isEditModalOpen = editTaskModal.style.display === "flex";
+
+    if (!isEditModalOpen) {
+      editTaskTitleInput.value = task.name;
+      editTaskDescriptionInput.value = task.description;
+      editTaskConclusionDateInput.value = task.conclusionDate;
+
+      editTaskModal.setAttribute("style", "display: flex;");
+      editTaskForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        handleEditNewTask(task)
+      });
+      
+    } else {
+      editTasktTitleError.textContent = "";
+      editTaskDescriptionError.textContent = "";
+      editTaskConclusionDateError.textContent = "";
+      
+      editTaskForm.reset();
+      editTaskModal.setAttribute("style", "display: none;");
+    }
+
+    return;
+  }
 }
 
 // List tasks
@@ -115,6 +209,7 @@ window.addEventListener("load", () => {
 
 function addToTaskList(task) {
   const taskItem = document.createElement("li");
+  taskItem.id = task.id;
 
   taskItem.innerHTML = `
     <div class="name_and_date">
@@ -122,8 +217,8 @@ function addToTaskList(task) {
       <p class="task_conclusion_date">Para: ${getFormattedDateString(task.conclusionDate)}</p>
     </div>
     <div class="buttons">
-      <button class="edit" id="edit-task">Editar</button>
-      <button class="delete" id="delete-task">Excluir</button>
+      <button class="edit">Editar</button>
+      <button class="delete">Excluir</button>
     </div>
   `;
 
@@ -156,7 +251,7 @@ function handleSearchTask(e) {
 // Delete task
 window.addEventListener("load", () => {
   const deleteTaskButtons = document.querySelectorAll("button.delete");
-  
+
   deleteTaskButtons.forEach((button) => {
     button.addEventListener("click", handleDeleteTask);
   });
@@ -173,3 +268,16 @@ function handleDeleteTask(e) {
 
   e.target.parentElement.parentElement.remove();
 }
+
+// Edit task
+window.addEventListener("load", () => {
+  const editTaskButtons = document.querySelectorAll("button.edit");
+  const tasks = JSON.parse(localStorage.getItem("tasks"));
+
+  editTaskButtons.forEach((button) => {
+    const taskId = button.parentElement.parentElement.id;
+    const task = tasks.find((task) => task.id == taskId);
+
+    button.addEventListener("click", () => toggleTaskModal("edit", task));
+  });
+});
